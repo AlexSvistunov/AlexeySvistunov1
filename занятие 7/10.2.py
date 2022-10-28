d4f1a16428ff4dd3b7e38a305809b6ae9dc003d2
AlexSvistunov <xdayx53@gmail.com>
2022-10-28 16:02:29 +0300

    внесены изменения

---
+++ занятие 7/10.2.py
@@ -0,0 +1,11 @@
+a = [54,31,65,356,74652,4234,24,576,2,21,76,43,142,1,4]
+print(' '.join([str(i) for i in a]))
+c = []
+for i in a:
+    if i < 10:
+        c.append(str(0))
+    if i > 20:
+        c.append(str(1))
+    if 10 <=i <= 20:
+        c.append(str(i))
+print(' '.join(c))
